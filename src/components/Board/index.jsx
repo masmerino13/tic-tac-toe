d4d83427e3d4d2ba7c859cc4a08e1ab3a44f305b
masmerino <masmerino@gmail.com>
2019-03-10 01:22:32 -0600

    Handle board size change

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -8,7 +8,17 @@ import Squares from '../Squares';
 class Board extends Component {
   state = {
     size: 3,
-    squares: Array(9).fill(null) // 3x3
+    squares: Array(9).fill(null) // 3X3
+  }
+
+  handleSizeChange = (e) => {
+    const { value } = e.target;
+    const nSquares = value * value;
+
+    this.setState({
+      size: value,
+      squares: Array(nSquares).fill(null) // sizeXsize
+    });
   }
 
   render() {
@@ -17,7 +27,7 @@ class Board extends Component {
     return (
       <BoardWrapper>
         <Scores scoreX={ 5 } />
-        <Config defaultSize={ size } />
+        <Config defaultSize={ size } onSizeChange={ this.handleSizeChange } />
         <Squares columns={ size } squares={ squares } onClicked={() => console.log('clic')} />
       </BoardWrapper>
     )
--- src/components/Config/index.jsx
+++ src/components/Config/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import { ConfigWrapper } from './config.styled';
 
-const Config = ({ defaultSize }) => {
+const Config = ({ defaultSize, onSizeChange }) => {
   return (
     <ConfigWrapper>
       <label>
-        Size: <input type="number" min="3" max="30" value={defaultSize} />
+        Size: <input type="number" min="3" max="30" value={defaultSize} onChange={onSizeChange} />
       </label>
       <label>
         <button className="btn btn-play" onClick={() => console.log('game started')}>Play now</button>

93cf79d993761e5069d278788ad37c15ad5f2c31
masmerino <masmerino@gmail.com>
2019-03-10 23:05:38 -0600

    Check winner horizontally

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { chain } from 'lodash'
 
 import { BoardWrapper } from './board.styled';
 import Scores from '../Scores';
@@ -10,7 +11,43 @@ class Board extends Component {
     size: 3,
     squares: Array(9).fill(null), // 3X3
     whoIsNext: 'x', // X start by default
-    steps: 0 // default
+    whoWon: null,
+    playing: false,
+    steps: 0, // default
+    scores: {
+      x: 0,
+      zero: 0,
+      draw: 0
+    }
+  }
+
+  checkWinner = () => {
+    const { size, squares } = this.state;
+    const winPathX = 'xxx';
+    const winPath0 = '000';
+    const horizontal = chain(squares)
+      .chunk(size)
+      .map(group => group.join(''))
+      .value();
+
+    horizontal.forEach(path => {
+      if (path.includes(winPathX)) {
+        this.setWinner('x');
+      } else if (path.includes(winPath0)) {
+        this.setWinner('zero');
+      }
+    });
+  }
+
+  setWinner(winner) {
+    const { scores } = this.state;
+    scores[winner] += 1;
+
+    this.setState({
+      playing: false,
+      whoWon: winner,
+      scores
+    });
   }
 
   handleSizeChange = (e) => {
@@ -28,10 +65,20 @@ class Board extends Component {
     const stepsCounter = steps + 1;
 
     squares[square] = whoIsNext;
+    
+    // -- calculate the winner
+    this.checkWinner();
+
+    if(squares.length === squares.filter(square => square).length)
+    {
+      this.setWinner('draw');
+    }
+    // --
 
     this.setState({
-      whoIsNext: whoIsNext === 'x' ? '0' : 'x',
-      squares
+      whoIsNext: whoIsNext === 'x' ? 'zero' : 'x',
+      squares,
+      steps: stepsCounter
     });
   }
 
@@ -46,14 +93,15 @@ class Board extends Component {
       size,
       squares,
       whoIsNext,
-      playing
+      playing,
+      scores
     } = this.state;
 
     return (
       <BoardWrapper>
         <Scores
           whoIsNext={ whoIsNext }
-          scoreX={ 5 } />
+          { ...scores } />
         <Config
           defaultSize={ size }
           onSizeChange={ this.handleSizeChange }
